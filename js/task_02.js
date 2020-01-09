487722fc00215a127d95120795ba1aecb29bdd96
sydoroknadiia <sydorok.nadiia@gmail.com>
2020-01-09 15:32:27 +0200

    finshed HW11 - task 2

---
+++ js/task_02.js
@@ -0,0 +1,35 @@
+'use strict'
+//-----// Задание 2 //-----//
+// Перепиши функцию toggleUserState() так, 
+// чтобы она не использовала callback-функцию callback, 
+// а принимала всего два параметра allUsers и userName и возвращала промис.
+
+const users = [
+  { name: 'Mango', active: true },
+  { name: 'Poly', active: false },
+  { name: 'Ajax', active: true },
+  { name: 'Lux', active: false },
+];
+
+const toggleUserState = (allUsers, userName) =>
+  new Promise(resolve =>
+    resolve(
+      allUsers.map(user =>
+        user.name === userName ? { ...user, active: !user.active } : user,
+      )
+    )
+  );
+
+const logger = updatedUsers => console.table(updatedUsers);
+
+/*
+ * Сейчас работает так
+ */
+// toggleUserState(users, "Mango", logger);
+// toggleUserState(users, "Lux", logger);
+
+/*
+ * Должно работать так
+ */
+toggleUserState(users, "Mango").then(logger);
+toggleUserState(users, "Lux").then(logger);
